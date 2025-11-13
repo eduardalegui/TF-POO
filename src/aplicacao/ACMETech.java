824a650eac5fee0fee6355e0a25e49175067a4cc
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-13 05:07:38 -0300

    começando a tecmologia

--- src/aplicacao/ACMETech.java
+++ src/aplicacao/ACMETech.java
@@ -1,13 +1,16 @@
 package src.aplicacao;
 
-import src.entidades.painel.PainelComprador;
+//import src.entidades.painel.PainelComprador;
 //import src.entidades.painel.PainelFornecedor;
+import src.entidades.painel.PainelTecnologia;
+
 
 public class ACMETech {
     public void inicializar() {}
 
     public void executar() {
-        PainelComprador minhaJanela = new PainelComprador();
+        //PainelComprador minhaJanela = new PainelComprador();
         //PainelFornecedor minhaJanela = new PainelFornecedor();
+        PainelTecnologia minhaJanela = new PainelTecnologia();
     }
 }
