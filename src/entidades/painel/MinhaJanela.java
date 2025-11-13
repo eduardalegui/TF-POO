07c4ed981c60e724d33474cc4a9338d41adf3509
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-13 04:12:16 -0300

    arrumei o cadastrar comprador

--- src/entidades/painel/MinhaJanela.java
+++ src/entidades/painel/MinhaJanela.java
@@ -12,7 +12,7 @@ public class MinhaJanela extends JFrame{
     
     public MinhaJanela(){
         super();
-        this.setSize(900,700);
+        this.setSize(1200,700);
         this.setTitle("Cadastrar Comprador"); // nome da janela
         setDefaultCloseOperation(EXIT_ON_CLOSE); //o codigo para de rodar quando clica para fechar a janela.
         
@@ -23,7 +23,7 @@ public class MinhaJanela extends JFrame{
             body.setLayout(layout);
             body.setBackground(corFundo);
             body.setOpaque(true); //não o torna tranparente
-            body.setBorder(BorderFactory.createEmptyBorder(25, 0, 0, 0));
+            body.setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
             
             //===============================================
                 JPanel comp1 = new JPanel();
@@ -32,21 +32,21 @@ public class MinhaJanela extends JFrame{
                 comp1.setLayout(layout11);
                 comp1.setBackground(Color.RED);
                 comp1.setOpaque(true);
-                comp1.setMaximumSize(new Dimension(900, 100));
-                titulo.setFont(new Font("SansSerif", Font.BOLD, 35));
+                comp1.setMaximumSize(new Dimension(1200, 150));
+                titulo.setFont(new Font("SansSerif", Font.BOLD, 50));
                 comp1.add(titulo);
             //===============================================
             //===============================================
                 JPanel comp2 = new JPanel();
                 GridLayout layout2 = new GridLayout(4,1);
                 comp2.setLayout(layout2);
-                comp2.setMaximumSize(new Dimension(600, 240));
+                comp2.setMaximumSize(new Dimension(900, 300));
                 comp2.setOpaque(false);
-                comp2.setBorder(BorderFactory.createEmptyBorder(25, 100, 0, 100));
+                comp2.setBorder(BorderFactory.createEmptyBorder(30, 140, 0, 140));
                 FlowLayout  layout1 = new FlowLayout(FlowLayout.RIGHT);
                 MatteBorder bordaCampTexto = new MatteBorder(0, 0, 2, 0, Color.RED);
-                Font fontDosTitulos = new Font("SansSerif", Font.BOLD, 20);
-                Font fontDosCampTexto = new Font("SansSerif", Font.PLAIN, 20);
+                Font fontDosTitulos = new Font("SansSerif", Font.BOLD, 30);
+                Font fontDosCampTexto = new Font("SansSerif", Font.PLAIN, 30);
 
                 //===============================================
                     JPanel comp21 = new JPanel();
@@ -113,65 +113,66 @@ public class MinhaJanela extends JFrame{
             //===============================================
                 JPanel comp3 = new JPanel();
                 JButton botao1= new JButton("Confirmar");
-                JButton botao2= new JButton("Mostrar");
-                JButton botao3= new JButton("Limpar");
-                JButton botao4= new JButton("Terminar");
+                JButton botao2= new JButton("Limpar");
+                JButton botao3= new JButton("Mostrar");
+                JButton botao4= new JButton("Fechar");
                 MatteBorder bordaBotoes = new MatteBorder(0, 5, 0, 5, corFundo);
-                Font fontBotoes = new Font("SansSerif", Font.BOLD, 18);
+                Font fontBotoes = new Font("SansSerif", Font.BOLD, 25);
                 FlowLayout  layout21 = new FlowLayout(FlowLayout.CENTER);
                 
-                botao1.setPreferredSize(new Dimension(120, 40));
+                botao1.setPreferredSize(new Dimension(150, 60));
                 botao1.setOpaque(true);
                 botao1.setBorder(bordaBotoes);
                 botao1.setBackground(Color.RED);
                 botao1.setFont(fontBotoes);
 
-                botao2.setPreferredSize(new Dimension(120, 40));
+                botao2.setPreferredSize(new Dimension(150, 60));
                 botao2.setOpaque(true);
                 botao2.setBorder(bordaBotoes);
                 botao2.setBackground(Color.RED);
                 botao2.setFont(fontBotoes);
 
-                botao3.setPreferredSize(new Dimension(120, 40));
+                botao3.setPreferredSize(new Dimension(150, 60));
                 botao3.setOpaque(true);
                 botao3.setBorder(bordaBotoes);
                 botao3.setBackground(Color.RED);
                 botao3.setFont(fontBotoes);
 
-                botao4.setPreferredSize(new Dimension(120, 40));
+                botao4.setPreferredSize(new Dimension(150, 60));
                 botao4.setOpaque(true);
                 botao4.setBorder(bordaBotoes);
                 botao4.setBackground(Color.RED);
                 botao4.setFont(fontBotoes);
 
                 comp3.setLayout(layout21);
-                comp3.setMaximumSize(new Dimension(900, 65));
+                comp3.setMaximumSize(new Dimension(900, 200));
                 comp3.setOpaque(false);
+                comp3.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
                 comp3.add(botao1);
                 comp3.add(botao2);
                 comp3.add(botao3);
                 comp3.add(botao4);
             //===============================================
             //===============================================
-                JPanel comp4 = new JPanel();
-                JTextArea areaTexto = new JTextArea(10, 45);
-                MatteBorder bordaAreaTexto = new MatteBorder(2, 2, 2, 2, Color.RED);
-                Font fontAreaTexto = new Font("SansSerif", Font.BOLD, 15);
-                areaTexto.setBorder(bordaAreaTexto);
-                areaTexto.setFont(fontAreaTexto);
-                areaTexto.setLineWrap(true);
-                areaTexto.setWrapStyleWord(true);
-                comp4.setLayout(layout11);
-                comp4.setMaximumSize(new Dimension(900, 210));
-                comp4.setOpaque(false);
-                JScrollPane scrollar = new JScrollPane(areaTexto);
-                comp4.add(scrollar);
+                // JPanel comp4 = new JPanel();
+                // JTextArea areaTexto = new JTextArea(10, 45);
+                // MatteBorder bordaAreaTexto = new MatteBorder(2, 2, 2, 2, Color.RED);
+                // Font fontAreaTexto = new Font("SansSerif", Font.BOLD, 15);
+                // areaTexto.setBorder(bordaAreaTexto);
+                // areaTexto.setFont(fontAreaTexto);
+                // areaTexto.setLineWrap(true);
+                // areaTexto.setWrapStyleWord(true);
+                // comp4.setLayout(layout11);
+                // comp4.setMaximumSize(new Dimension(1200, 210));
+                // comp4.setOpaque(false);
+                // JScrollPane scrollar = new JScrollPane(areaTexto);
+                // comp4.add(scrollar);
             //===============================================
 
             body.add(comp1);
             body.add(comp2);
             body.add(comp3);
-            body.add(comp4);
+            //body.add(comp4);
         //===============================================
         this.add(body);
         setVisible(true); // ultima linha!!!
