ac9a75894dd8f64a8fd2b5c7ab8526057674655d
eduardalegui <eduarda.leguisao@edu.pucrs.br>
2025-11-18 01:26:15 -0300

    limpei o codigo

--- src/aplicacao/ACMETech.java
+++ src/aplicacao/ACMETech.java
@@ -1,19 +1,14 @@
 package src.aplicacao;
 
+//import src.painel.PainelComprador;
 //import src.painel.PainelFornecedor;
 //import src.painel.PainelTecnologia;
-//import src.painel.FornecedorPe;
-//import src.painel.TecnologiaLu;
-import src.painel.PainelComprador;
-
 
 public class ACMETech {
-    public void inicializar() {}
 
     public void executar() {
-        PainelComprador minhaJanela = new PainelComprador();
+        //PainelComprador minhaJanela = new PainelComprador();
         //PainelFornecedor minhaJanela = new PainelFornecedor();
-        //TecnologiaLu minhaJanela = new TecnologiaLu();
         //PainelTecnologia minhaJanela = new PainelTecnologia();
     }
 }
