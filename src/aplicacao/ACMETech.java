8d37ef265bd5b21c84580fa2ac08b526f312a781
eduardalegui <eduarda.leguisao@edu.pucrs.br>
2025-11-19 01:11:11 -0300

    coloquei a descrição das paginas na ordem que eu estou fazendo

--- src/aplicacao/ACMETech.java
+++ src/aplicacao/ACMETech.java
@@ -1,19 +1,19 @@
 package src.aplicacao;
 
-import src.painel.PainelCadastrarVenda;
+//import src.painel.PainelCadastrarVenda;
 //import src.painel.PainelComprador;
 //import src.painel.PainelFornecedor;
-//import src.painel.PainelHomePage;
+import src.painel.PainelHomePage;
 //import src.painel.PainelTecnologia;
 
 
 public class ACMETech {
 
     public void executar() {
-        PainelCadastrarVenda minhaJanela = new PainelCadastrarVenda();
+        //PainelCadastrarVenda minhaJanela = new PainelCadastrarVenda();
         //PainelComprador minhaJanela = new PainelComprador();
         //PainelFornecedor minhaJanela = new PainelFornecedor();
-        //PainelHomePage minhaJanela = new PainelHomePage();
+        PainelHomePage minhaJanela = new PainelHomePage();
         //PainelTecnologia minhaJanela = new PainelTecnologia();
     }
 }
