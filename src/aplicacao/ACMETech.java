4806c767eb182975429372164845d04c0b6aebd6
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-20 19:25:21 -0300

    arrumei o ACMETech

--- src/aplicacao/ACMETech.java
+++ src/aplicacao/ACMETech.java
@@ -18,7 +18,15 @@ import src.entidades.Fornecedor;
 import src.entidades.Participante;
 import src.entidades.Tecnologia;
 import src.entidades.Venda;
-
+// import src.ui.PainelHomePage;
+// import src.ui.comprador.PainelCadastrarComprador;
+// import src.ui.comprador.PainelComprador;
+// import src.ui.fornecedor.PainelCadastrarFornecedor;
+// import src.ui.fornecedor.PainelFornecedor;
+// import src.ui.tecnologia.PainelCadastrarTecnologia;
+// import src.ui.tecnologia.PainelTecnologia;
+// import src.ui.venda.PainelCadastrarVenda;
+// import src.ui.venda.PainelVenda;
 public class ACMETech {
     private CatalogoParticipantes participantes;
 
@@ -41,6 +49,15 @@ public class ACMETech {
 
     public void executar() {
         inicializar();
+        // PainelHomePage minhaJanela = new PainelHomePage();
+        // PainelCadastrarComprador minhaJanela = new PainelCadastrarComprador();
+        // PainelComprador minhaJanela = new PainelComprador();
+        // PainelCadastrarFornecedor minhaJanela = new PainelCadastrarFornecedor();
+        // PainelFornecedor minhaJanela = new PainelFornecedor();
+        // PainelCadastrarTecnologia minhaJanela = new PainelCadastrarTecnologia();
+        // PainelTecnologia minhaJanela = new PainelTecnologia();
+        // PainelCadastrarVenda minhaJanela = new PainelCadastrarVenda();
+        // PainelVenda minhaJanela = new PainelVenda();
     }
 
     public List<String> lerArquivoParticipantes(String caminho) {
