8a365338a2f5c368bdc8d006b06840b220b2cc2f
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-23 04:03:14 -0300

    terminei o relatorio comprador

--- src/ui/comprador/DialogRelatorioComprador.java
+++ src/ui/comprador/DialogRelatorioComprador.java
@@ -1,12 +1,13 @@
 package src.ui.comprador;
-import src.entidades.CatalogoParticipantes;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.util.*;
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Dimension;
-import java.util.*;
-
+import src.entidades.CatalogoParticipantes;
 
 
 public class DialogRelatorioComprador extends JDialog{
@@ -14,41 +15,42 @@ public class DialogRelatorioComprador extends JDialog{
     public DialogRelatorioComprador(CatalogoParticipantes catalogoParticipantes){
         super();
         this.catalogoParticipantes = catalogoParticipantes;
-        this.setSize(700,500);
-        this.setTitle("Relatório Comprador"); // nome da janela
-        //setDefaultCloseOperation(EXIT_ON_CLOSE); //o codigo para de rodar quando clica para fechar a janela.
-        
+        this.setSize(700,500);            
         Color corFundo = new Color(238, 236, 194);
         Color vermelho = new Color(233, 37, 37);
-        //JButton voltar = new JButton("Voltar");
-        Font fontBotoes = new Font("SansSerif", Font.BOLD, 20);
+        Font fontBotoes = new Font("SansSerif", Font.BOLD, 30);
         MatteBorder bordaBotaoVoltar = new MatteBorder(2,2, 2, 2, vermelho);
-        ////FlowLayout  layoutB = new FlowLayout(FlowLayout.LEFT);
         JPanel botaoVoltar = new JPanel();
         BoxLayout layout = new BoxLayout(botaoVoltar, BoxLayout.Y_AXIS);
-        //GridBagLayout layout11 = new GridBagLayout();
+        GridBagLayout layout11 = new GridBagLayout();
         botaoVoltar.setMaximumSize(new Dimension(700, 500));
         botaoVoltar.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        //voltar.setPreferredSize(new Dimension(100, 40));
         botaoVoltar.setOpaque(true);
-        //voltar.setBorder(bordaBotaoVoltar);
         botaoVoltar.setLayout(layout);
-        //voltar.setFont(fontBotoes);
-        //voltar.setBackground(corFundo);
-        //botaoVoltar.add(voltar);
         botaoVoltar.setBackground(corFundo);
-
-                JPanel comp1 = new JPanel();
-                JLabel titulo = new JLabel("RELATÓRIO COMPRADOR");
-                //comp1.setLayout(layout11);
-                comp1.setBackground(vermelho);
-                comp1.setOpaque(true);
-                comp1.setMaximumSize(new Dimension(700, 40));
-                titulo.setFont(new Font("SansSerif", Font.BOLD, 10));
-                comp1.add(titulo);
-
-        this.add(comp1);
-        //botaoVoltar.add(comp1);
+        JPanel comp1 = new JPanel();
+        JLabel titulo = new JLabel("RELATÓRIO COMPRADOR");
+        comp1.setBackground(vermelho);
+        comp1.setOpaque(true);
+        comp1.setMaximumSize(new Dimension(700, 40));
+        titulo.setFont(new Font("SansSerif", Font.BOLD, 23));
+        comp1.add(titulo);
+        JPanel comp4 = new JPanel();
+        JTextArea areaTexto = new JTextArea(20, 55);
+        areaTexto.setText(relatorio());
+        MatteBorder bordaAreaTexto = new MatteBorder(2, 2, 2, 2, vermelho);
+        Font fontAreaTexto = new Font("SansSerif", Font.BOLD, 15);
+        areaTexto.setBorder(bordaAreaTexto);
+        areaTexto.setFont(fontAreaTexto);
+        areaTexto.setLineWrap(true);
+        areaTexto.setWrapStyleWord(true);
+        comp4.setLayout(layout11);
+        areaTexto.setBackground(corFundo);
+        comp4.setOpaque(false);
+        JScrollPane scrollar = new JScrollPane(areaTexto);
+        comp4.add(scrollar);
+        botaoVoltar.add(comp1);
+        botaoVoltar.add(comp4);
         this.add(botaoVoltar);
         this.setLocationRelativeTo(null);
         setVisible(true); // ultima linha!!!
@@ -57,5 +59,13 @@ public class DialogRelatorioComprador extends JDialog{
     public List<String> mostrarRelatorio(){
         return catalogoParticipantes.mostrarRelatorioDeCompradores();
     }
+
+    public String relatorio(){
+        String retorno = "";
+        for(String s : mostrarRelatorio()){
+            retorno = retorno + s + "\n";
+        }
+        return retorno;
+    } 
 
 }
