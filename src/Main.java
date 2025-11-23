450a2d9ac6d8517a32cbec45c56b4f8cab9ad973
PedroHCastilhos <pedro.castilhos.fraga@gmail.com>
2025-11-23 14:14:32 -0300

    Colocando dados coerentes

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {  //trabalhinho legal do yama!
     public static void main(String args[]) {
         ACMETech aplicacao = new ACMETech();
         aplicacao.executar();
-        
     }               //trabalhinho legal do yama!
 }                   //trabalhinho legal do yama!
                     //trabalhinho legal do yama!
