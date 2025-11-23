432c9b55eed2800dd1d21857f62931de07bb0d2d
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-23 04:03:54 -0300

    terminei o relatorio tecnologia

--- src/ui/tecnologia/DialogRelatorioTecnologia.java
+++ src/ui/tecnologia/DialogRelatorioTecnologia.java
@@ -1,17 +1,57 @@
 package src.ui.tecnologia;
 
+import java.awt.*;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
-import java.awt.event.ActionListener;
-import java.util.*;
-import src.entidades.*;
+import src.entidades.CatalogoParticipantes;
+import src.entidades.Fornecedor;
+import src.entidades.Participante;
 
 
 public class DialogRelatorioTecnologia extends JDialog{
     private CatalogoParticipantes catalogoParticipantes;
-
     public DialogRelatorioTecnologia(CatalogoParticipantes catalogoParticipantes){
+        super();
         this.catalogoParticipantes = catalogoParticipantes;
+        this.setSize(700,500);            
+        Color corFundo = new Color(238, 236, 194);
+        Color azul = new Color(0, 100, 250);
+        Font fontBotoes = new Font("SansSerif", Font.BOLD, 30);
+        MatteBorder bordaBotaoVoltar = new MatteBorder(2,2, 2, 2, azul);
+        JPanel botaoVoltar = new JPanel();
+        BoxLayout layout = new BoxLayout(botaoVoltar, BoxLayout.Y_AXIS);
+        GridBagLayout layout11 = new GridBagLayout();
+        botaoVoltar.setMaximumSize(new Dimension(700, 500));
+        botaoVoltar.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        botaoVoltar.setOpaque(true);
+        botaoVoltar.setLayout(layout);
+        botaoVoltar.setBackground(corFundo);
+        JPanel comp1 = new JPanel();
+        JLabel titulo = new JLabel("RELATÓRIO TECNOLOGIA");
+        comp1.setBackground(azul);
+        comp1.setOpaque(true);
+        comp1.setMaximumSize(new Dimension(700, 40));
+        titulo.setFont(new Font("SansSerif", Font.BOLD, 23));
+        comp1.add(titulo);
+        JPanel comp4 = new JPanel();
+        JTextArea areaTexto = new JTextArea(20, 55);
+        MatteBorder bordaAreaTexto = new MatteBorder(2, 2, 2, 2, azul);
+        Font fontAreaTexto = new Font("SansSerif", Font.BOLD, 15);
+        areaTexto.setBorder(bordaAreaTexto);
+        areaTexto.setFont(fontAreaTexto);
+        areaTexto.setLineWrap(true);
+        areaTexto.setWrapStyleWord(true);
+        comp4.setLayout(layout11);
+        areaTexto.setBackground(corFundo);
+        comp4.setOpaque(false);
+        JScrollPane scrollar = new JScrollPane(areaTexto);
+        comp4.add(scrollar);
+        botaoVoltar.add(comp1);
+        botaoVoltar.add(comp4);
+        this.add(botaoVoltar);
+        this.setLocationRelativeTo(null);
+        setVisible(true); // ultima linha!!
     }
     
     public List<String> mostrarRelatorio(){
