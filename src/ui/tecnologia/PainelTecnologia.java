73a1c891277515471bace66a3de343e9c0c6179b
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-11-25 00:29:55 -0300

    arrumei o layout de PainelTecnologia

--- src/ui/tecnologia/PainelTecnologia.java
+++ src/ui/tecnologia/PainelTecnologia.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 import src.entidades.CatalogoParticipantes;
 import src.ui.HomePage;
@@ -16,137 +17,132 @@ public class PainelTecnologia extends JPanel implements ActionListener {
     private JButton botao24;
     private HomePage home;
     private CatalogoParticipantes catalogoParticipantes;
+    private JPanel botaoVoltar;
+    private JPanel comp1;
+    private JPanel comp2;
+    private JPanel comp21;
+    private JPanel comp22;
+    private JPanel comp23;
+    private JPanel comp24;
+    private Color corFundo;
+    private Color azul;
+    private BoxLayout layout;
+    private FlowLayout  layoutB;
+    private FlowLayout  layout11;
+    private MatteBorder bordaBotao;
+    private MatteBorder bordaTF;
+    private Font fontBotoes;
+    private Font fontTF;
+    private EmptyBorder bordaComp;
+    private Dimension dimensao;
+    private JLabel tituloTF;
 
-    public PainelTecnologia(HomePage home, CatalogoParticipantes catalogoParticipantes) {
+    public PainelTecnologia(HomePage home, CatalogoParticipantes catalogoParticipantes){
         super();
         this.catalogoParticipantes = catalogoParticipantes;
+        this.home = home;
         this.setSize(1200,700);
-        //this.setTitle("Tecnologia"); // nome da janela
-        //setDefaultCloseOperation(EXIT_ON_CLOSE); //o codigo para de rodar quando clica para fechar a janela.
-        Color corFundo = new Color(238, 236, 194);
-        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
-        this.setBackground(corFundo);
-        this.setLayout(layout);
+        this.setMinimumSize(new Dimension(1200, 700));
+        corFundo = new Color(238, 236, 194);
+        azul = new Color(0, 100, 250);
+        layout = new BoxLayout(this, BoxLayout.Y_AXIS);
         //===============================================
-            this.home = home;
-            //JPanel body = new JPanel();
-            //BoxLayout layout = new BoxLayout(body, BoxLayout.Y_AXIS);
-            //Color corFundo = new Color(238, 236, 194);
-            Color azul = new Color(0, 100, 250);
+            fontBotoes = new Font("SansSerif", Font.BOLD, 25);
+            bordaBotao = new MatteBorder(2,2, 2, 2, azul);
+            layoutB = new FlowLayout(FlowLayout.LEFT);
             voltar = new JButton("Voltar");
-            Font fontBotoes = new Font("SansSerif", Font.BOLD, 25);
-            MatteBorder bordaBotaoVoltar = new MatteBorder(2,2, 2, 2, azul);
-            FlowLayout  layoutB = new FlowLayout(FlowLayout.LEFT);
-            FlowLayout  layout11 = new FlowLayout(FlowLayout.CENTER);
-            JPanel botaoVoltar = new JPanel();
-            botaoVoltar.setMinimumSize(new Dimension(1200, 60));
-            botaoVoltar.setBorder(BorderFactory.createEmptyBorder(10, 10, 70, 0));
             voltar.setPreferredSize(new Dimension(100, 40));
-            botaoVoltar.setOpaque(false);
-            voltar.setOpaque(true);
-            voltar.setBorder(bordaBotaoVoltar);
+            voltar.setBorder(bordaBotao);
             voltar.setLayout(layoutB);
-            botaoVoltar.setLayout(layoutB);
             voltar.setFont(fontBotoes);
             voltar.setBackground(corFundo);
+            botaoVoltar = new JPanel();
+            botaoVoltar.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
+            botaoVoltar.setLayout(layoutB);
             botaoVoltar.add(voltar);
-            // body.add(botaoVoltar);
-            // body.setLayout(layout);
-            // body.setBackground(corFundo);
-            // body.setOpaque(true); //não o torna tranparente
-            // body.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-
-            //===============================================
-                JPanel comp1 = new JPanel();
-                MatteBorder bordaTF = new MatteBorder(0, 0, 4, 0, azul);
-                JLabel tituloTF = new JLabel("Métodos de Tecnologia");
-                Font fontTF = new Font("SansSerif", Font.BOLD, 60);
-                tituloTF.setBorder(bordaTF);
-                tituloTF.setFont(fontTF);
-                comp1.add(tituloTF);
-                comp1.setLayout(layout11);
-                comp1.setSize(1200,40);
-                comp1.setBorder(BorderFactory.createEmptyBorder(80, 0, 0, 0));
-                comp1.setOpaque(false);
-            //===============================================
-            //===============================================
-                JPanel comp2 = new JPanel();
-                comp2.setLayout(layout11);
-                comp2.setMaximumSize(new Dimension(800, 200));
-                comp2.setBorder(BorderFactory.createEmptyBorder(20, 0, 400, 0));
-
-                //===============================================
-                    JPanel comp21 = new JPanel();
-                    botao21= new JButton("Cadastrar");
-                    MatteBorder bordaBotoes21 = new MatteBorder(2, 2, 2, 2, azul);
-                    botao21.setPreferredSize(new Dimension(250, 100));
-                    botao21.setOpaque(true);
-                    botao21.setBorder(bordaBotoes21);
-                    botao21.setBackground(azul);
-                    botao21.setFont(fontBotoes);
-                    comp21.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 25));
-                    comp21.add(botao21);
-                    comp21.setOpaque(false);
-                //===============================================
-                //===============================================
-                    JPanel comp22 = new JPanel();
-                    botao22= new JButton("Mostrar Relatório");
-                    MatteBorder bordaBotoes22 = new MatteBorder(2, 2, 2, 2, azul);
-                    botao22.setPreferredSize(new Dimension(250, 100));
-                    botao22.setOpaque(true);
-                    botao22.setBorder(bordaBotoes22);
-                    botao22.setBackground(azul);
-                    botao22.setFont(fontBotoes);
-                    comp22.setBorder(BorderFactory.createEmptyBorder(0, 25, 20, 0));
-                    comp22.add(botao22);
-                    comp22.setOpaque(false);
-                //===============================================
-                //===============================================
-                    JPanel comp23 = new JPanel();
-                    botao23= new JButton("Consultar Maior");
-                    MatteBorder bordaBotoes23 = new MatteBorder(2, 2, 2, 2, azul);
-                    botao23.setPreferredSize(new Dimension(250, 100));
-                    botao23.setOpaque(true);
-                    botao23.setBorder(bordaBotoes23);
-                    botao23.setBackground(azul);
-                    botao23.setFont(fontBotoes);
-                    comp23.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 25));
-                    comp23.add(botao23);
-                    comp23.setOpaque(false);
-                //===============================================
-                //===============================================
-                    JPanel comp24 = new JPanel();
-                    botao24= new JButton("Definir Fornecedor");
-                    MatteBorder bordaBotoes24 = new MatteBorder(2, 2, 2, 2, azul);
-                    botao24.setPreferredSize(new Dimension(250, 100));
-                    botao24.setOpaque(true);
-                    botao24.setBorder(bordaBotoes24);
-                    botao24.setBackground(azul);
-                    botao24.setFont(fontBotoes);
-                    comp24.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
-                    comp24.add(botao24);
-                    comp24.setOpaque(false);
-                //===============================================
-
-                comp2.add(comp21);
-                comp2.add(comp22);
-                comp2.add(comp23);
-                comp2.add(comp24);
-                comp2.setOpaque(false);
-            //===============================================
-            botao21.addActionListener(this);
-            botao22.addActionListener(this);
-            botao24.addActionListener(this);
-            voltar.addActionListener(this);
-            //==============================================
-            //body.add(comp1);
-            //body.add(comp2);
+            botaoVoltar.setOpaque(false);
         //===============================================
-        //this.add(body);
+        //=============================================== 
+            fontTF = new Font("SansSerif", Font.BOLD, 60);
+            layout11 = new FlowLayout(FlowLayout.CENTER);
+            bordaTF = new MatteBorder(0, 0, 4, 0, azul);
+            tituloTF = new JLabel("Métodos de Tecnologia");
+            tituloTF.setBorder(bordaTF);
+            tituloTF.setFont(fontTF);
+            comp1 = new JPanel();
+            comp1.setLayout(layout11);
+            comp1.setSize(1200,40);
+            comp1.setBorder(BorderFactory.createEmptyBorder(80, 0, 0, 0));
+            comp1.setOpaque(false);
+            comp1.add(tituloTF);
+        //===============================================
+        //===============================================
+            comp2 = new JPanel();
+            comp2.setLayout(layout11);
+            comp2.setMaximumSize(new Dimension(800, 200));
+            comp2.setBorder(BorderFactory.createEmptyBorder(20, 0, 400, 0));
+            bordaComp = new EmptyBorder(0,25,20,25);
+            dimensao = new Dimension(250, 100);
+            //===============================================
+                botao21= new JButton("Cadastrar");
+                botao21.setPreferredSize(dimensao);
+                botao21.setBorder(bordaBotao);
+                botao21.setBackground(azul);
+                botao21.setFont(fontBotoes);
+                comp21 = new JPanel();
+                comp21.setBorder(bordaComp);
+                comp21.add(botao21);
+                comp21.setOpaque(false);
+            //===============================================
+            //===============================================
+                botao22= new JButton("Mostrar Relatório");
+                botao22.setPreferredSize(dimensao);
+                botao22.setBorder(bordaBotao);
+                botao22.setBackground(azul);
+                botao22.setFont(fontBotoes);
+                comp22 = new JPanel();
+                comp22.setBorder(bordaComp);
+                comp22.add(botao22);
+                comp22.setOpaque(false);
+            //===============================================
+            //===============================================
+                botao23= new JButton("Definir Fornecedor");
+                botao23.setPreferredSize(dimensao);
+                botao23.setBorder(bordaBotao);
+                botao23.setBackground(azul);
+                botao23.setFont(fontBotoes);
+                comp23 = new JPanel();
+                comp23.setBorder(bordaComp);
+                comp23.add(botao23);
+                comp23.setOpaque(false);
+            //===============================================
+            //===============================================
+                botao24= new JButton("Consultar Maior");
+                botao24.setPreferredSize(dimensao);
+                botao24.setBorder(bordaBotao);
+                botao24.setBackground(azul);
+                botao24.setFont(fontBotoes);
+                comp24 = new JPanel();
+                comp24.setBorder(bordaComp);
+                comp24.add(botao24);
+                comp24.setOpaque(false);
+            //===============================================
+            comp2.add(comp21);
+            comp2.add(comp22);
+            comp2.add(comp23);
+            comp2.add(comp24);
+            comp2.setOpaque(false);
+        //===============================================
+        botao21.addActionListener(this);
+        botao22.addActionListener(this);
+        botao23.addActionListener(this);
+        botao24.addActionListener(this);
+        voltar.addActionListener(this);
+        this.setBackground(corFundo);
+        this.setLayout(layout);
         this.add(botaoVoltar);
         this.add(comp1);
         this.add(comp2);
-        //this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
@@ -160,6 +156,8 @@ public class PainelTecnologia extends JPanel implements ActionListener {
             home.mudaPainel(0);
         } else if(e.getSource() == botao24) { //consultar maior
             new DialogMaiorValorTecnologia(catalogoParticipantes);
+        }else if(e.getSource() == botao23) { //Definir Fornecedor
+            
         }
     }
 }
